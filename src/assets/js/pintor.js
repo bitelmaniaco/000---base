265a59180dc67011c2a78eaa7e37d2847aa041e4
bitelmaniaco <m.martindominguez@icloud.com>
2022-01-20 11:45:18 +0100

    Relleno de assets
    Relleno básico de los assets

---
+++ src/assets/js/pintor.js
@@ -0,0 +1,12 @@
+class Pintor {
+    nombre = '';
+    apellidos = '';
+    edad = '';
+
+    constructor(nom, ape, ed){
+        this.nombre = nom;
+        this.apellidos = ape;
+        this.edad = ed;
+    }
+
+}
